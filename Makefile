c99851f368ad2b601268736276c8e77a9f6e7317
Tamara Temple <tamouse@gmail.com>
2012-04-15 13:40:02 -0500

    finish restructure, add makefile

---
+++ Makefile
@@ -0,0 +1,12 @@
+OBJECTS=cookbook/extracallouts cookbook/extracallouts.php cookbook/extracallouts/LICENSE.txt cookbook/extracallouts/README.md pub/extracallouts/extracallouts.css pub/extracallouts/quote-2A2AFF.png pub/extracallouts/quote-2AFFD5.png pub/extracallouts/quote-66FF00.png pub/extracallouts/quote-7F2AFF.png pub/extracallouts/quote-C837AB.png pub/extracallouts/quote-D35F5F.png pub/extracallouts/quote-FF7F2A.png pub/extracallouts/quote-black.png pub/extracallouts/quote-grey.png pub/extracallouts/quote.gif pub/extracallouts/quote.svg
+
+INSTALL_DIR=../..
+
+dist: $(OBJECTS)
+	zip -r extracallouts.zip cookbook/ pub/
+
+clean:
+	find . -name '*~' -print -delete
+
+install: extracallouts.zip
+	unzip extracallouts.zip -d $(INSTALL_DIR)
--- cookbook/extracallouts.php
+++ cookbook/extracallouts.php
@@ -1,19 +1,18 @@
-<?php if (!defined('PmWiki')) exit(); // -*- mode: php; time-stamp-start: "version [\"<]"; time-stamp-format: "%Y-%3b-%02d %02H:%02M"; -*- 
+<?php if (!defined('PmWiki')) exit(); 
 /**
  *
- * mycallouts
+ * extracallouts
  *
  * @author Tamara Temple <tamara@tamaratemple.com>
  * @since 2011/11/01
- * @version <2011-Nov-15 04:03>
  * @copyright (c) 2011 Tamara Temple Web Development
  * @license GPLv3
  *
  */
+$RecipeInfo['ExtraCallouts']['Version'] = '2012-04-15';
 
-SDV($HTMLHeaderFmt['mycallouts'], "
-  <link rel='stylesheet' href='$PubDirUrl/css/mycallouts/mycallouts.css' 
-    type='text/css' />
+SDV($HTMLHeaderFmt['extracallouts'], "
+  <link rel='stylesheet' href='$PubDirUrl/extracallouts/extracallouts.css' type='text/css' />
 ");
 
 SDVA($WikiStyle['quote'], array('class'=>'round lrindent quote'));
